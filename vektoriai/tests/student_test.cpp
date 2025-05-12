00ae404f4d2dd23b43010b99fe3077912325a9a8
Emilė Kėsaitė <emakkesa@gmail.com>
2025-05-12 20:20:07 +0300

    building unit tests

--- vektoriai/tests/student_test.cpp
+++ vektoriai/tests/student_test.cpp
@@ -1,29 +1,32 @@
 #define CATCH_CONFIG_MAIN
 #include "catch.hpp"
-#include "../vektoriai/students.h"
+#include "../students.h"
+#include "../Zmogus.h"
 
 
 TEST_CASE("Studentas constructor and getters") {
-    Studentas s("Jonas", "Jonaitis", {8, 9, 10}, 9);
-    s.pazymys_vidurkis();
-    s.pazymys_mediana();
+    auto zm = make_unique<Studentas>("Jonas", "Jonaitis", vector<int>{8, 9, 10}, 9);
+    Studentas* s = dynamic_cast<Studentas*>(zm.get()); 
+    s->pazymys_vidurkis();
+    s->pazymys_mediana();
 
-    REQUIRE(s.getVardas() == "Jonas");
-    REQUIRE(s.getPavarde() == "Jonaitis");
-    REQUIRE(s.getEgzaminas() == 9);
-    REQUIRE(s.getHw().size() == 3);
-    REQUIRE(s.getPazVid() == 8.0);
-    REQUIRE(s.getPazM() == 8.0);
+    REQUIRE(zm->getVardas() == "Jonas");
+    REQUIRE(zm->getPavarde() == "Jonaitis");
+    REQUIRE(s->getEgzaminas() == 9);
+    REQUIRE(s->getHw().size() == 3);
+    REQUIRE(s->getPazVid() == 8.0);
+    REQUIRE(s->getPazM() == 8.0);
 }
 
 TEST_CASE("The Rule of Five") {
-    Studentas s1("Petras", "Petraitis", {5, 9, 10, 8},7);
+    auto zm1 = make_unique<Studentas>("Petras", "Petraitis", vector<int>{5, 9, 10, 8},7);
+    Studentas* s1 = dynamic_cast<Studentas*>(zm1.get()); 
 
-    Studentas copy(s1); // Copy constructor
+    Studentas copy(*s1); // Copy constructor
     REQUIRE(copy.getVardas() == "Petras");
     
     Studentas s2;
-    s2 = s1;
+    s2 = *s1;
     REQUIRE(s2.getEgzaminas() == 8);
 
     Studentas s3("Saule", "Saulyte", {6, 4, 8}, 4);
